a2619106f57195b22d53cab9d291675f25a7b399
luisbboca12 <107447827+luisbboca12@users.noreply.github.com>
2024-01-02 23:38:23 -0300

    Add files via upload

---
+++ primera entrega proyecto final/src/app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const {Server}= require('socket.io');
+const exphbs = require('express-handlebars').create;
+const path = require('path');
+
+///importaciones
+const {productRouter,productManager} = require('./rutas/products');
+const {cartsRouter} = require('./rutas/carts');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+/// Inciar servidor
+const serverExpress = app.listen(PORT, () => {console.log(`Servidor Express iniciado en http://localhost:${PORT}`)});
+const io = new Server(serverExpress)
+
+// Configuración de Handlebars
+app.engine('.handlebars', exphbs({ extname: '.handlebars' }).engine);
+app.set('view engine', '.handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas
+
+app.use('/api/products', productRouter);
+app.use('/api/carts', cartsRouter);
+
+// Ruta para la vista index.handlebars
+app.get('/', (req, res) => {
+  const allProducts = productManager.getAllProducts();
+  res.render('index', { products: allProducts });
+});
+
+// Ruta para la vista en tiempo real con websockets
+app.get('/realtimeproducts', (req, res) => {
+  const allProducts = productManager.getAllProducts();
+  res.render('realTimeProducts', { products: allProducts });
+});
+
+// Configuración de WebSocket
+io.on('connection', (socket) => {
+  console.log('Usuario conectado');
+
+// Escucha eventos desde el cliente
+socket.on('productoNuevo', () => {
+  // Actualiza la lista de productos
+  io.emit('actualizarProductos', productManager.getAllProducts());
+  });
+
+  socket.on('productoEliminado', () => {
+  // Actualiza la lista de productos 
+    io.emit('actualizarProductos', productManager.getAllProducts());
+  });
+  
+  // Maneja la desconexión del usuario
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado');
+  });
+});
+
+// Manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Algo salió mal!');
+});
